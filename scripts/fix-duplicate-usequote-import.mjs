37d9e8249d072e459e069323f60b762500e6474e
rucafonso33-debug <rucafonso33@gmail.com>
2026-07-23 20:31:06 +0100

    Remove temporary import cleanup script

--- scripts/fix-duplicate-usequote-import.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs';
-const file = 'src/App.tsx';
-let source = fs.readFileSync(file, 'utf8');
-source = source.replace(/(?:import \{ useQuote \} from '\.\/hooks\/useQuote';\n)+/, "import { useQuote } from './hooks/useQuote';\n");
-fs.writeFileSync(file, source);
